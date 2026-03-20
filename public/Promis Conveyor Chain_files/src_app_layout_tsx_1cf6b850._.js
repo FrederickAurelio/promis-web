32df9f8acbba4a7070b2776d2a5b7b8ebdaae638
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2026-03-20 21:00:18 +0800

    chore: remove unused file

--- public/Promis Conveyor Chain_files/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_7e7d016d._.js",
-  "static/chunks/src_3044260f._.js",
-  "static/chunks/[root-of-the-server]__0f0ba101._.css"
-],
-    source: "dynamic"
-});
